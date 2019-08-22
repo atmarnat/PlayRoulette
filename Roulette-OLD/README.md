ab51ee3fb3bf840be0c1a2492c5c0206c28c93cb
atmar <nathaniel.atmar@outlook.com>
2019-08-22 08:49:23 -0700

    Had to get rid of the .git ,files working correctly

---
+++ Roulette-OLD/README.md
@@ -0,0 +1,2 @@
+# Roulette
+To run, clone in GitHub, open the project file in the solution folder, and then debug
---
+++ Roulette-OLD/Roulette/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TheGame;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t$~$~$~$~$~$~$");
+            Console.WriteLine("\t\t\t\t\t$ ROULETTE! $");
+            Console.WriteLine("\t\t\t\t\t$~$~$~$~$~$~$\n");
+
+            Play.Game();
+
+            Console.WriteLine("\n\t\t\t\t   $~$~$~$~$~$~$~$~$~$~$~$~$");
+            Console.WriteLine("\t\t\t\t   $ Thank's for playing!! $");
+            Console.WriteLine("\t\t\t\t   $~$~$~$~$~$~$~$~$~$~$~$~$\n");
+        }
+    }
+}
